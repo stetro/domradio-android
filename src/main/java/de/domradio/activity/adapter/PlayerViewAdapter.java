d2a518e32406396e7b3d6bb403a28469a2b47d81
Steffen Tröster <steffen.troester@googlemail.com>
2015-09-01 18:54:55 +0200

    display and animate radio stream infos

--- src/main/java/de/domradio/activity/adapter/PlayerViewAdapter.java
+++ src/main/java/de/domradio/activity/adapter/PlayerViewAdapter.java
@@ -4,6 +4,7 @@ package de.domradio.activity.adapter;
 import android.app.Activity;
 import android.content.Intent;
 import android.support.design.widget.FloatingActionButton;
+import android.view.View;
 import android.widget.TextView;
 
 import de.domradio.R;
@@ -12,9 +13,11 @@ import de.domradio.activity.util.PlayButtonOnClickListener;
 import de.domradio.service.EventBusCallback;
 import de.domradio.service.RadioService;
 import de.domradio.service.RadioServiceState;
+import de.domradio.service.TitleRefreshRunnable;
 import de.domradio.service.event.RadioStartedEvent;
 import de.domradio.service.event.RadioStartingEvent;
 import de.domradio.service.event.RadioStoppedEvent;
+import de.domradio.service.event.UpdatePlayerTitleEvent;
 import de.greenrobot.event.EventBus;
 
 
@@ -22,19 +25,26 @@ public class PlayerViewAdapter implements ViewAdapter {
 
     private FloatingActionButton playerButton;
     private TextView playerInfoText;
+    private TextView playerTitleText;
+    private TitleRefreshRunnable titleRefreshThread;
+    private MainActivity activity;
 
     @Override
     public void register(MainActivity activity) {
+        this.activity = activity;
         bindViews(activity);
         updatePlayerState();
         startRadioService(activity);
         EventBus.getDefault().register(this);
+        titleRefreshThread = new TitleRefreshRunnable();
+        new Thread(titleRefreshThread).start();
     }
 
     private void bindViews(MainActivity activity) {
         playerButton = (FloatingActionButton) activity.findViewById(R.id.radio_fragment_button);
         playerButton.setOnClickListener(new PlayButtonOnClickListener(activity));
         playerInfoText = (TextView) activity.findViewById(R.id.radio_fragment_text);
+        playerTitleText = (TextView) activity.findViewById(R.id.radio_fragment_title);
     }
 
     @Override
@@ -42,9 +52,9 @@ public class PlayerViewAdapter implements ViewAdapter {
         if (RadioService.get_state().equals(RadioServiceState.STOPPED)) {
             activity.stopService(new Intent(activity, RadioService.class));
         }
+        titleRefreshThread.setRunning(false);
         EventBus.getDefault().unregister(this);
     }
-
 
     private void startRadioService(Activity activity) {
         Intent intent = new Intent(activity.getApplicationContext(), RadioService.class);
@@ -84,6 +94,17 @@ public class PlayerViewAdapter implements ViewAdapter {
     @EventBusCallback
     public void onEvent(RadioStoppedEvent e) {
         updatePlayerState();
+    }
+
+    @EventBusCallback
+    public void onEvent(final UpdatePlayerTitleEvent e) {
+        activity.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                playerTitleText.setText(e.getStation().onair.title + " - " + e.getStation().onair.artist);
+                playerTitleText.setVisibility(View.VISIBLE);
+            }
+        });
     }
 
 }
